b4295a0d9a37e845aab19411ecb2d3d5f6f966f1
Doãn Tuấn <coder@Coder-MacBook-Pro.local>
2016-11-08 08:46:38 +0700

    Done UI and run

--- LightON/ViewController.swift
+++ LightON/ViewController.swift
@@ -8,10 +8,29 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    var check :Bool!
+    let avDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+    
+    
+    @IBOutlet weak var buttonLight: UIButton!
+    
+    @IBAction func tapLight(_ sender: UIButton) {
+        if !check
+        {
+            buttonLight.setImage(UIImage(named: "lightOn.png"), for: UIControlState.normal)
+            check = true
+        }else
+        {
+            buttonLight.setImage(UIImage(named: "lightOff.png"), for: UIControlState.normal)
+            check = false
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        check = false
         // Do any additional setup after loading the view, typically from a nib.
     }
 
