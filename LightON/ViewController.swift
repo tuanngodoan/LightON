f8288e4294ceca96cb0d563a8473a85173c110e5
Coder123 <coder@coder-macbook-pro.local>
2016-11-08 07:59:18 +0700

    Initial Commit

---
+++ LightON/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LightON
+//
+//  Created by Doãn Tuấn on 11/8/16.
+//  Copyright © 2016 doantuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
